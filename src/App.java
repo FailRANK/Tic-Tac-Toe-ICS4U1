bf7dcf2695b2d734ec4d5c102dbd15da21cb5868
FailRANK <franknguyen2006@gmail.com>
2024-05-03 11:17:43 -0400

    Added win condition

--- src/App.java
+++ src/App.java
@@ -1,10 +1,11 @@
 import javax.swing.JOptionPane;
 
 public class App {
+    static boolean End = false;
     public static void main(String[] args) throws Exception {
         String[] type = {" ", " ", " ", " ", " ", " ", " ", " ", " "};
         int team = 0; 
-        while (true) {
+        while (End != true) {
             if (team == 0)
                 System.out.println("X turn");
             else
@@ -22,17 +23,73 @@ public class App {
             }
 
             JOptionPane.showMessageDialog(null, type[0] + "|" + type[1] + "|" + type[2] + "\n-------" + "\n" + type[3] + "|" + type[4] + "|" + type[5] + "\n-------" + "\n" + type[6] + "|" + type[7] + "|" + type[8]);
-
-            System.out.println(type[0] + "|" + type[1] + "|" + type[2]);
-            System.out.println("-----");
-            System.out.println(type[3] + "|" + type[4] + "|" + type[5]);
-            System.out.println("-----");
-            System.out.println(type[6] + "|" + type[7] + "|" + type[8]);
             
             if (team == 0)
                 team = 1;
             else
                 team = 0;
+
+            wincheck(type, "X");
+            wincheck(type, "O");
+
+            for(int i = 0; i < 9; i++) {
+                if (type[i] == " ") {
+                    break;
+                }
+                if (i == 8) {
+                    JOptionPane.showMessageDialog(null, "Draw");
+                    return; 
+                }
+            } 
+            
+        }
+    }
+
+    static void wincheck(String[] type, String team) {
+        if (type[0] == team && type[1] == team && type[2] == team) {
+            JOptionPane.showMessageDialog(null, team + " wins");
+            End = true;
+            return;
+        }
+        if (type[3] == team && type[4] == team && type[5] == team) {
+            JOptionPane.showMessageDialog(null, team + " wins");
+            End = true;
+            return;
+        }
+        if (type[6] == team && type[7] == team && type[8] == team) {
+            JOptionPane.showMessageDialog(null, team + " wins");
+            End = true;
+            return;
+        }
+        if (type[0] == team && type[3] == team && type[6] == team) {
+            JOptionPane.showMessageDialog(null, team + " wins");
+            End = true;
+            return;
+        }
+        if (type[0] == team && type[3] == team && type[6] == team) {
+            JOptionPane.showMessageDialog(null, team + " wins");
+            End = true;
+            return;
+        }
+        if (type[1] == team && type[4] == team && type[7] == team) {
+            JOptionPane.showMessageDialog(null, team + " wins");
+            End = true;
+            return;
+        }
+        if (type[2] == team && type[5] == team && type[8] == team) {
+            JOptionPane.showMessageDialog(null, team + " wins");
+            End = true;
+            return;
+        }
+        if (type[0] == team && type[4] == team && type[8] == team) {
+            JOptionPane.showMessageDialog(null, team + " wins");
+            End = true;
+            return;
+        }
+        if (type[2] == team && type[4] == team && type[6] == team) {
+            JOptionPane.showMessageDialog(null, team + " wins");
+            End = true;
+            return;
         }
     }
 }
